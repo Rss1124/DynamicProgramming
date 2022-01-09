222356438e435ff0e5f466da5e15332c6e10066e
Rs1124 <604347974@qq.com>
2022-01-09 13:17:45 +0800

    今日动态规划训练： 题目：gridTraveler_使用表格策略

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ using namespace std;
 
 long long int Fib_byTree(int n, long long int array[]); //斐波那契数列
 long long int Fib_byTable(long long int array[],int n);
-long long int gridTraveler(int m,int r,int c,long long int array[]); //gridTraveler m表示二维数组的列数，r、c分别表示当前结点所处的行与列
+long long int gridTraveler_byTree(int m,int r,int c,long long int array[]); //gridTraveler m表示二维数组的列数，r、c分别表示当前结点所处的行与列
+long long int gridTraveler_byTable(int r,int c,long long int **array); //r、c分别表示当前结点所处的行与列
 int canAndHowSum(int target,int array[],int cS[],int n,int targetArray[],int m,int i,int &min,int tempArray[],int &tag); //canSum，target表示目标值，array[]内的元素用来凑target，cS是用来存储重复信息的仓库，targetArray是组成target的元素集合，m用来记录当前递归的深度
 int Sort(int a[],int low,int high);
 void QuickSort(int a[],int low,int high);
@@ -24,20 +25,32 @@ int main() {
 //    cout<<Fib(50,fA)<<endl;
 
 /** 斐波那契数列_表格策略 **/
-    long long int array[255]={};
-    array[1]=1;
-    int n=50;
-    cout<<Fib_byTable(array,n);
+//    long long int array[255]={};
+//    array[1]=1;
+//    int n=50;
+//    cout<<Fib_byTable(array,n);
 
-/** gridTraveler **/
+/** gridTraveler_byTree **/
 //    int r,c,n,m;
 //    cout<<"请输入二维数组的行列:";
 //    cin>>r>>c;
 //    n=r*c;
 //    m=c;
 //    long long int *gTA=new long long int[n](); //gridTravelerArray
-//    cout<<gridTraveler(m,r-1,c-1,gTA)<<endl; //m=3,r=2,c=3,n=6
+//    cout<<gridTraveler_byTree(m,r-1,c-1,gTA); //m=3,r=2,c=3,n=6
 //    return 0;
+
+/** gridTraveler_byTable **/
+    int r,c;
+    long long int **array; //array[r][c]
+    cout<<"请输入二维数组的行列:";
+    cin>>r>>c;
+    array = new long long int*[r];
+    for(int i=0;i<r;i++){
+        array[i]= new long long int[c]{}; //二维数组初始化
+    }
+    array[0][0]=1;
+    cout<<gridTraveler_byTable(r,c,array);
 
 /** BestSum **/
 //    int target,n,m=0,i=0,min,tag=0;
@@ -107,12 +120,24 @@ long long int Fib_byTable(long long int array[],int n){
     return array[n];
 }
 
-long long int gridTraveler(int m,int r,int c, long long int array[]){  //m=3,r=1,c=2
+long long int gridTraveler_byTree(int m,int r,int c, long long int array[]){  //m=3,r=1,c=2
     if(array[r*m+c]!=0&&(r>=0&&c>=0)) return array[r*m+c]; //二维数组表示一维数组：r*m+c
     if(r==0&&c==0) return array[r*m+c]=1;
     if(r<0||c<0) return 0;
-    array[r*m+c]=gridTraveler(m,r,c-1,array)+gridTraveler(m,r-1,c,array);
+    array[r*m+c]=gridTraveler_byTree(m,r,c-1,array)+gridTraveler_byTree(m,r-1,c,array);
     return array[r*m+c];
+}
+
+long long int gridTraveler_byTable(int r,int c,long long int **array){
+    for (int i=0;i<r;i++)
+    {
+        for (int j=0;j<c;j++)
+        {
+            if(i+1<r) array[i+1][j]=array[i+1][j]+array[i][j];
+            if(j+1<c) array[i][j+1]=array[i][j+1]+array[i][j];
+        }
+    }
+    return array[r-1][c-1];
 }
 
 int canAndHowSum(int target,int array[],int cS[],int n,int targetArray[],int m,int i,int &min,int tempArray[],int &tag){
